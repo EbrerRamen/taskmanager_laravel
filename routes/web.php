dc2d3442370e50df19f5717d3797020a16788efe
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-09 02:30:00 +0600

    added route for editing task

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ Route::get('/', [TaskController::class, 'index']);
 // Add a new task
 Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 
+// Show the edit form for a task
+Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+
 // Mark task as complete/incomplete
 Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
 
