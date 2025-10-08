235c2337c03935aee6078f8844d15547ccebd135
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-08 18:45:59 +0600

    routing setup for show, add, mark, and delete task

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Show all tasks
+Route::get('/', [TaskController::class, 'index']);
+
+// Add a new task
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+
+// Mark task as complete/incomplete
+Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+
+// Delete a task
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
